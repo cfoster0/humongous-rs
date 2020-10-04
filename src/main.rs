317b2c931a104eed2cc9bf786fb1873675a89b1f
Charles Foster <cfoster0@ccrma.stanford.edu>
2020-10-03 23:57:19 -0700

    Filtering WARCs for just responses, HTTP body extraction, character encoding detection, UTF-8 conversion, and language detection implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+
+use std::collections::{HashMap, HashSet};
+use bytes::Bytes;
+
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::io::{Error, ErrorKind};
 use futures::stream::{self, Stream, StreamExt, BoxStream};
 use async_compression::stream::GzipDecoder;
 use std::io::Result;
-use bytes::Bytes;
 use nom::{Err, IResult, Needed};
 use warc_parser::{record, records};
 use reqwest::{get, Response};
+use futures::future::join_all;
 use tokio::prelude::*;
+
 use log::{trace, debug, info, warn, error};
 use env_logger;
 use bumpalo::Bump;
+
+use whatlang::{detect_lang};
+use httparse;
+use chardet::charset2encoding;
+use encoding::DecoderTrap;
+use encoding::label::encoding_from_whatwg_label;
+use ammonia::Builder;
 
 pub type Warc = warc_parser::Record;
 pub struct WarcError();
@@ -22,8 +34,8 @@ pub enum MyResult<T, E> {
 }
 
 pub enum WarcResult {
-    WarcOk(Warc),
-    WarcErr(WarcError),
+    Ok(Warc),
+    Err(WarcError),
 }
 
 impl From<Result<Bytes>> for MyResult<Bytes, ()> {
@@ -94,7 +106,7 @@ impl Stream for WarcDecoder<'_> {
                     // Byte stream has an error
                     Poll::Ready(Some(_)) => {
                         error!("An error occurred!");
-                        return Poll::Ready(Some(WarcResult::WarcErr(WarcError())))
+                        return Poll::Ready(Some(WarcResult::Err(WarcError())))
                     },
                     // Byte stream not yet ready
                     Poll::Pending => break,
@@ -124,7 +136,7 @@ impl Stream for WarcDecoder<'_> {
         match me.records.pop() {
             // Take a record from the queue
             Some(record) => {
-                return Poll::Ready(Some(WarcResult::WarcOk(record)));
+                return Poll::Ready(Some(WarcResult::Ok(record)));
             },
             // Nothing left in the record queue
             None => {
@@ -171,33 +183,106 @@ pub async fn get_compressed_warc_records(url: &str) -> Option<WarcDecoder<'stati
     }
 }
 
+pub fn http_body(mut warc: Warc) -> String {
+    // WARC-Type is required by the spec, so this will panic if it is not included
+    let warc_type = warc.headers.get("WARC-Type").unwrap();
+    if warc_type != "response" {
+        return "".to_string();
+    }
+    let mut headers = [httparse::EMPTY_HEADER; 32];
+
+    let body_index = match httparse::Response::new(&mut headers).parse(warc.content.as_slice()) {
+        Ok(httparse::Status::Complete((body_index))) => {
+            Some(body_index)
+        },
+        _ => {
+            None
+        },
+    };
+    if let Some(i) = body_index {
+        let body = warc.content.split_off(i);
+
+        let result = chardet::detect(body.as_slice());
+        // result.0 Encode
+        // result.1 Confidence
+        // result.2 Language
+
+        debug!("{:?}: {:?}", result.0, result.1);
+
+        // decode file into utf-8
+        let coder = encoding_from_whatwg_label(charset2encoding(&result.0));
+        if coder.is_some() {
+            match coder.unwrap().decode(&body, DecoderTrap::Ignore) {
+                Ok(text) => return text.to_string(),
+                _ => (),
+            }
+        }
+    }
+    return String::new();
+}
+
+pub fn tag_language(text: String) -> (String, Option<whatlang::Lang>) {
+    let sanitized_text = ammonia::Builder::default().allowed_classes(HashMap::new()).tags(HashSet::new()).clean(&text).to_string();
+    let language = {
+        if sanitized_text.len() > 25 {
+            let lang = detect_lang(&sanitized_text);
+            info!("Language detected: {:?}", lang);
+            lang
+        } else {
+            None
+        }
+    };
+    return (text, language);
+}
+
 #[tokio::main]
 pub async fn main() {
     env_logger::init();
 
-    let handle = tokio::spawn(async move {
-        //let stream_option = get_uncompressed_warc_records("https://github.com/webrecorder/warcio/raw/master/test/data/example.warc").await;
-        //let stream_option = get_uncompressed_warc_records("https://raw.githubusercontent.com/sbeckeriv/warc_nom_parser/master/sample/plethora.warc").await;
-        //let stream_option = get_uncompressed_warc_records("https://raw.githubusercontent.com/sbeckeriv/warc_nom_parser/master/sample/bbc.warc").await;
+    let urls = vec![
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00000.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00001.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00002.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00003.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00004.warc.gz",
+        "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00005.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00006.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00007.warc.gz",
+        //"https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735792.85/warc/CC-MAIN-20200803083123-20200803113123-00008.warc.gz",
+        //
+        //"https://archive.org/download/warc-www.hifimuseum.de-2018-11-26/www.hifimuseum.de_2018-11-26-00000.warc.gz",
+        //"https://github.com/webrecorder/warcio/raw/master/test/data/example.warc.gz",
+    ];
 
-        //let stream_option = get_compressed_warc_records("https://github.com/webrecorder/warcio/raw/master/test/data/example.warc.gz").await;
-        let stream_option = get_compressed_warc_records("https://archive.org/download/warc-www.hifimuseum.de-2018-11-26/www.hifimuseum.de_2018-11-26-00000.warc.gz").await;
-        //let stream_option = get_compressed_warc_records("https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2020-34/segments/1596439735810.18/warc/CC-MAIN-20200803111838-20200803141838-00363.warc.gz").await;
-        match stream_option {
-            Some(stream) => {
-                info!("Stream successfully connected!");
-                //let count = stream.fuse().fold(0, |acc, x| async move { acc + 1 }).await;
-                //println!("Number of WARC records: {:?}", count);
-                let collection = stream.fuse().collect::<Vec<WarcResult>>().await;
-                info!("Number of WARC records: {:?}", collection.len());
-                return ();
-            },
-            None => {
-                error!("Stream was not successfully connected.");
-                return ();
+
+    let handles = urls.iter().map(|&url| { 
+        tokio::spawn(async move {
+            //let stream_option = get_uncompressed_warc_records("https://github.com/webrecorder/warcio/raw/master/test/data/example.warc").await;
+            //let stream_option = get_uncompressed_warc_records("https://raw.githubusercontent.com/sbeckeriv/warc_nom_parser/master/sample/plethora.warc").await;
+            //let stream_option = get_uncompressed_warc_records("https://raw.githubusercontent.com/sbeckeriv/warc_nom_parser/master/sample/bbc.warc").await;
+
+            let stream_option = get_compressed_warc_records(url).await;
+            match stream_option {
+                Some(stream) => {
+                    info!("Stream successfully connected from {:?}!", url);
+                    let count = stream.map(|x| {
+                        match x {
+                            WarcResult::Ok(x) => http_body(x),
+                            _ => "".to_string(),
+                        }
+                    }).map(|x| tag_language(x)).fuse().fold(0u32, |acc, x| async move { acc + 1 }).await;
+                    println!("Number of WARC records: {:?}", count);
+                    //let collection = stream.fuse().collect::<Vec<WarcResult>>().await;
+                    //info!("Number of WARC records from {:?}: {:?}", url, collection.len());
+                    return ();
+                },
+                None => {
+                    error!("Stream was not successfully connected.");
+                    return ();
+                }
             }
-        }
+        })
     });
 
-    handle.await;
+    join_all(handles).await;
 }
